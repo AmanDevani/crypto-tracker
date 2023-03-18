1f77ea40c0c5cbd1fd8046681a5de40cc48c0863
Aman <amandevani25@gmail.com>
2023-03-18 09:59:57 +0530

    crypto-app

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='container'>
+      Here You can see the list of Exchanges and also see the list of coins with their price,chart in different currency.
+    </div>
+  )
+}
+
+export default Home
